c7ad5817303a7f101ae52a28c656f45809be9c68
CHOON XIANG YEOH - STUDENT <g00425056@atu.ie>
2023-03-08 15:37:20 +0000

    base

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,22 @@
-//Sean, Rick, Micheal
+//Rick,Michael,Sean
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<time.h>
 
-#include <stdio.h>
+int main()
+{
+	char a[9] = { ' ',' ',' ',' ',' ',' ',' ',' ',' ' };
+
+	printf("Tic-tac-toe\n\n");
+	printf("   |   |   \n");
+	printf(" %c | %c | %c \n", a[0], a[1], a[2]);
+	printf("___|___|___\n");
+	printf("   |   |   \n");
+	printf(" %c | %c | %c \n", a[3], a[4], a[5]);
+	printf("___|___|___\n");
+	printf("   |   |   \n");
+	printf(" %c | %c | %c \n", a[6], a[7], a[8]);
+	printf("   |   |   \n");
+	return 0;
+}
