6c55e643191d2ee89c7a81d6a9eaf832cb1ecb25
Yi Zheng <g00425075@atu.ie>
2023-03-22 14:41:50 +0000

    bot

--- Main.cpp
+++ Main.cpp
@@ -4,11 +4,13 @@
 #include<conio.h>
 #include<time.h>
 
+int move(char arr[]);
 
 int main()
 {
-	int box, i;
-	char a[9] = { ' ',' ',' ',' ',' ',' ',' ',' ',' ' };
+	srand(time(0));
+	int box, i, opponent;
+	char a[9] = { ' ',' ',' ',' ',' ',' ',' ',' ',' '};
 
 
 
@@ -29,72 +31,97 @@ int main()
 
 		printf("Please pick a box: ");
 		scanf_s("%i", &box);
-		switch (box)
-		{
-		case 11:
-		{
-			a[0] = 'x';
-			break;
-		}
-		case 12:
-		{
-			a[1] = 'x';
-			break;
-		}
-		case 13:
-		{
-			a[2] = 'x';
-			break;
-		}
-		case 21:
-		{
-			a[3] = 'x';
-			break;
-		}
-		case 22:
-		{
-			a[4] = 'x';
-			break;
-		}
-		case 23:
-		{
-			a[5] = 'x';
-			break;
-		}
-		case 31:
-		{
-			a[6] = 'x';
-			break;
-		}
-		case 32:
-		{
-			a[7] = 'x';
-			break;
-		}
-		case 33:
-		{
-			a[8] = 'x';
-			break;
-		}
+		
+			switch (box)
+			{
+			case 11:
+			{
+				a[0] = 'x';
+				break;
+			}
+			case 12:
+			{
+				a[1] = 'x';
+				break;
+			}
+			case 13:
+			{
+				a[2] = 'x';
+				break;
+			}
+			case 21:
+			{
+				a[3] = 'x';
+				break;
+			}
+			case 22:
+			{
+				a[4] = 'x';
+				break;
+			}
+			case 23:
+			{
+				a[5] = 'x';
+				break;
+			}
+			case 31:
+			{
+				a[6] = 'x';
+				break;
+			}
+			case 32:
+			{
+				a[7] = 'x';
+				break;
+			}
+			case 33:
+			{
+				a[8] = 'x';
+				break;
+			}
+			}
+			opponent = move(a);
+
+			if (opponent == 0)
+			{
+				opponent = move(a);
+			}
+
+			else
+			{
+				a[opponent] = 'O';
+			}
+
+			printf("Tic-tac-toe\n\n");
+			printf(" 1   2   3\n");
+			printf("1  |   |   \n");
+			printf(" %c | %c | %c \n", a[0], a[1], a[2]);
+			printf("___|___|___\n");
+			printf("2  |   |   \n");
+			printf(" %c | %c | %c \n", a[3], a[4], a[5]);
+			printf("___|___|___\n");
+			printf("3  |   |   \n");
+			printf(" %c | %c | %c \n", a[6], a[7], a[8]);
 		}
 
-
-		printf("Tic-tac-toe\n\n");
-		printf(" 1   2   3\n");
-		printf("1  |   |   \n");
-		printf(" %c | %c | %c \n", a[0], a[1], a[2]);
-		printf("___|___|___\n");
-		printf("2  |   |   \n");
-		printf(" %c | %c | %c \n", a[3], a[4], a[5]);
-		printf("___|___|___\n");
-		printf("3  |   |   \n");
-		printf(" %c | %c | %c \n", a[6], a[7], a[8]);
-
-
-	}
-
-	return 0;
+	return 0; 
 }
 
+	int move(char arr[9])
+{
+	int i, b;
+	for (i = 0; i <= 9; i++)
+	{
+		b = rand() % 9;
+		if (arr[b] == 'x')
+		{
+			return 0;
+		}
 
+		else
+		{
+			return b;
+		}
+	}
 
+}
