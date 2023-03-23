53f22468ede56d87f606399f3518e2d1d2defa5a
Sean Maloney <G00420811@atu.ie>
2023-03-23 18:45:25 +0000

    Final version

--- Main.cpp
+++ Main.cpp
@@ -1,38 +1,48 @@
 //Rick,Michael,Sean
 #include<stdio.h>
-#include<stdlib.h>
+#include<stdlib.h>// Libaries
 #include<conio.h>
 #include<time.h>
 
 int move(char arr[]);
-int check(char arr[]);
+int checker(char arr[],  int b);// functions
 
 int main()
 {
 	srand(time(0));
-	int box, i, opponent, check;
+	int box, i, opponent, check, c; //intergeres we used
 	char a[9] = { ' ',' ',' ',' ',' ',' ',' ',' ',' ' };
+	char str[30], ch;
 
-
+	
 
 	printf("Tic-tac-toe\n\n");
 	printf(" 1 | 2 | 3 \n");
 	printf(" %c | %c | %c \n", a[0], a[1], a[2]);
 	printf("___|___|___\n");
 	printf(" 4 | 5 | 6 \n");
-	printf(" %c | %c | %c \n", a[3], a[4], a[5]);
+	printf(" %c | %c | %c \n", a[3], a[4], a[5]);// prints boxes at start
 	printf("___|___|___\n");
 	printf(" 7 | 8 | 9 \n");
 	printf(" %c | %c | %c \n", a[6], a[7], a[8]);
 	printf("   |   |   \n");
-
-
-	for (i = 0; i <= 9; i++)
-	{
+	//printf("Please enter your name:");
+	//scanf_s("%[^\n]s", &str);
+	//ch = getchar();
+	//printf("%s", str);
+	for (i = 0; i <= 1; i+=0)
+	{	
 		printf("Please pick a box: ");
-		scanf_s("%i", &box);
-
-		switch (box)
+		scanf_s("%i", &box);// lets user slect a box
+		c = checker(a, box);
+		while (c == 1)
+		{
+			printf("The space is taken\n");
+			printf("Please pick a box: "); // result of checker, blocks user from placing on taken spot
+			scanf_s("%i", &box);
+			c = checker(a, box);
+		}
+		switch (box)// switch statement to place x's 
 		{
 		case 1:
 		{
@@ -80,24 +90,45 @@ int main()
 			break;
 		}
 		}
-
-		opponent = move(a);
-
-		if (opponent == 0)
-		{
-			opponent = move(a);
-		}
-
-		else
-		{
-			a[opponent] = 'o';
-		}
-
 		printf(" 1 | 2 | 3 \n");
 		printf(" %c | %c | %c \n", a[0], a[1], a[2]);
 		printf("___|___|___\n");
 		printf(" 4 | 5 | 6 \n");
-		printf(" %c | %c | %c \n", a[3], a[4], a[5]);
+		printf(" %c | %c | %c \n", a[3], a[4], a[5]); //updated list for player
+		printf("___|___|___\n");
+		printf(" 7 | 8 | 9 \n");
+		printf(" %c | %c | %c \n", a[6], a[7], a[8]);
+		printf("   |   |   \n");
+		if (a[0] == 'x' && a[1] == 'x' && a[2] == 'x' || a[3] == 'x' && a[4] == 'x' && a[5] == 'x' ||// this scans the x's and see if a 3 in a row is made
+			a[6] == 'x' && a[7] == 'x' && a[8] == 'x' || a[0] == 'x' && a[3] == 'x' && a[6] == 'x' ||
+			a[1] == 'x' && a[4] == 'x' && a[7] == 'x' || a[2] == 'x' && a[5] == 'x' && a[8] == 'x' ||
+			a[0] == 'x' && a[4] == 'x' && a[8] == 'x' || a[2] == 'x' && a[4] == 'x' && a[6] == 'x')
+		{
+			printf("you win :)");
+			i = 10;
+		}
+		else if (a[0] == 'o' && a[1] == 'o' && a[2] == 'o' || a[3] == 'o' && a[4] == 'o' && a[5] == 'o' ||
+			a[6] == 'o' && a[7] == 'o' && a[8] == 'o' || a[0] == 'o' && a[3] == 'o' && a[6] == 'o' ||// this scans the o's and see if a 3 in a row is made
+			a[1] == 'o' && a[4] == 'o' && a[7] == 'o' || a[2] == 'o' && a[5] == 'o' && a[8] == 'o' ||
+			a[0] == 'o' && a[4] == 'o' && a[8] == 'o' || a[2] == 'o' && a[4] == 'o' && a[6] == 'o')
+		{
+			printf("you lose :(");
+			i = 2;
+		}
+		opponent = move(a);// allows bot to make a move
+		while (opponent == 10)
+		{
+			opponent = move(a);
+		}
+		{
+			a[opponent] = 'o';
+		}
+		printf("Computer's turn\n");
+		printf(" 1 | 2 | 3 \n");
+		printf(" %c | %c | %c \n", a[0], a[1], a[2]);
+		printf("___|___|___\n");
+		printf(" 4 | 5 | 6 \n");
+		printf(" %c | %c | %c \n", a[3], a[4], a[5]);// updated list for bot
 		printf("___|___|___\n");
 		printf(" 7 | 8 | 9 \n");
 		printf(" %c | %c | %c \n", a[6], a[7], a[8]);
@@ -116,40 +147,37 @@ int main()
 			a[0] == 'o' && a[4] == 'o' && a[8] == 'o' || a[2] == 'o' && a[4] == 'o' && a[6] == 'o')
 		{
 			printf("you lose :(");
-			i = 10;
+			i = 2;
 		}
 	}
 	return 0;
 }
 
-
 int move(char arr[9])
 {
 	int i, b;
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i <= 1; i+=0)
 	{
 		b = rand() % 9;
-		if (arr[b] == 'x')
+		if (arr[b] != ' ')//ranomizer
 		{
-			return 0;
+			return 10;
 		}
-
-		else if (arr[b] == 'o')
-		{
-			return 0;
-		}
-
 		else
 		{
 			return b;
 		}
 	}
-
-
-<<<<<<< HEAD
-	return 0;
 }
 
-=======
+int checker(char arr[9], int b)
+{
+	if (arr[b - 1] != ' ')
+	{
+		return 1;
+	} //checks if a space is taken or not
+	else
+	{
+		return 0;
+	}
 }
->>>>>>> 4bea0023aaf74df1302983e13f463ff807cb4a6b
